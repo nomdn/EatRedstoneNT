ca24a024c35856102750abeb140fd3b9e7bd8394
wsmdn <14669850+wsmdn@user.noreply.gitee.com>
2025-06-07 11:49:11 +0000

    update apps/gushici.py. 少打一个等于号.....
    Signed-off-by: wsmdn <14669850+wsmdn@user.noreply.gitee.com>

--- apps/gushici.py
+++ apps/gushici.py
@@ -7,19 +7,16 @@ import base64
 username = input("输入你这次的用户名")
 print(f"你好！{username}")
 
-
 def base64_encode(score):
-    #分数格式先改成字符串再改成utf8编码
+    # 分数格式先改成字符串再改成utf8编码
     score_bytes = str(score).encode('utf-8')
     encoded_score = base64.b64encode(score_bytes)
     return encoded_score.decode('utf-8')
 
-
 def base64_decode(encoded_score):
     score_bytes = encoded_score.encode('utf-8')
-    decoded_score = base64.b64decode(score_bytes)#解密同理
+    decoded_score = base64.b64decode(score_bytes)  # 解密同理
     return int(decoded_score.decode('utf-8'))
-
 
 def update_user_score(user, score):
     # 尝试打开文件，如果文件不存在则创建
@@ -40,7 +37,6 @@ def update_user_score(user, score):
     with open('user_score.json', 'w') as file:
         json.dump(user_score, file, indent=4)
 
-
 try:
     with open('user_score.json', 'r') as file:
         # 读取现有的分数数据
@@ -54,7 +50,7 @@ except FileNotFoundError:
     # 文件不存在，创建一个空的分数字典
     user_scores = {}
 
-#下面的内容应该是个人就能看懂
+# 下面的内容应该是个人就能看懂
 a = (requests.get(f"https://xiaoapi.cn/API/game_gs.php?msg=开始游戏&id={username}")).text
 print(a)
 right = 0
@@ -73,7 +69,7 @@ for i in range(1, 50):
                 b = (requests.get(f"https://xiaoapi.cn/API/game_gs.php?msg=答2&id={username}")).text
                 right += 1
             elif answer == 3:
-                b = (requests.get(f"https://xiaoapi.cn/API/game_gs.php?msg=答3&id{username}")).text
+                b = (requests.get(f"https://xiaoapi.cn/API/game_gs.php?msg=答3&id={username}")).text
                 right += 1
             elif answer == 4:
                 b = (requests.get(f"https://xiaoapi.cn/API/game_gs.php?msg=答4&id={username}")).text
@@ -90,7 +86,6 @@ for i in range(1, 50):
     print(b)
 
 # 示例使用
-
 update_user_score(username, right)
 
 print("来让我看看！")
